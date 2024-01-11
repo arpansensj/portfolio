f871ab26bb15a8aada88b236691c3f41d9ed2df2
Arpan Sen <sen.arpan12@gmail.com>
2024-01-11 16:44:01 +0530

    Minor Changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 const Hero = () => {
   return (
     <div className="flex items-center justify-evenly pt-10">
+      {/* <img src="/assets/bg.svg" alt="" className="absolute flex"/> */}
         <h1 className="text-[#42446E] font-bold text-5xl">
   Hi👋, <br/>My name is <br/> <span className="bg-gradient-to-r from-pink-500 to-sky-500 text-transparent bg-clip-text font-bold text-5xl">Arpan Sen</span> <br/> I build things for web.
 </h1>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,15 +11,15 @@ const headerData = {
 
 const Navbar = () => {
   return (
-    <nav className=" text-grey-800 py-4 px-6 flex items-center justify-between">
+    <nav className=" text-grey-800 py-4 px-6 flex items-center justify-between sticky top-0 bg-white">
       <div className="flex items-center justify-center">
         <img src={headerData.logo.src} alt={headerData.logo.alt} className="w-20 h-20 rounded-full mr-4" />
         <h1 className="text-2xl font-bold">{headerData.title}</h1>
       </div>
       <ul className="flex items-center space-x-6">
         {headerData.navigation.map((item) => (
-          <li key={item.text} className="hover:text-gray-300">
-            <a href={item.link} className="px-3 py-2 rounded-md font-medium">{item.text}</a>
+          <li key={item.text} className="">
+            <a href={item.link} className="hover:bg-gradient-to-r hover:from-pink-500 hover:to-sky-500 px-3 py-2 rounded-full font-medium hover:text-white">{item.text}</a>
           </li>
         ))}
       </ul>

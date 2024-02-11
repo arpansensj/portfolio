12b77fe5689efac00bfe3059dd5391b41caf41c7
Arpan Sen <sen.arpan12@gmail.com>
2024-02-12 02:45:13 +0530

    Added 2nd project and some minor changes

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,14 +1,14 @@
-const Cards = () => {
+const Cards = ({link, imgSrc, title}) => {
   return (
     <div className=" bg-white lg:w-[600px] lg:p-5 lg:shadow-xl shadow-xl lg:hover:shadow-2xl lg:rounded-3xl rounded-lg p-2">
-      <a href="https://trip-booking.vercel.app/" target='_blank'>
+      <a href={link} target='_blank'>
         <img
           className="lg:rounded-3xl rounded-lg border-2 border-bg-gradient-to-r from-pink-500 to-sky-500"
-          src="/assets/Project1.png"
+          src={imgSrc}
           alt=""
         />
         <h2 className=" text-[#42446E] text-center sm:text-xl font-bold p-2 text-lg">
-          Trip Booking Page
+          {title}
         </h2>
         <h3></h3>
       </a>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,14 +3,23 @@ import Cards from "./Cards";
 const Projects = () => {
   return (
     <div className="" id="Projects">
-      <h2 className="text-[#42446E] pt-20 lg:font-bold lg:text-3xl font-bold text-2xl text-center lg:pt-28 pt-12">
+      <h2 className="text-[#42446E] pt-20 lg:font-bold lg:text-3xl font-bold text-2xl text-center lg:pt-28">
         Projects
       </h2>
       <h3 className="text-[#666] text-center lg:pt-6 pt-3 lg:text-2xl md:text-2xl text-xl">
         Thing I've built so far
       </h3>
       <div className="flex justify-center items-center flex-wrap lg:pt-4 pt-2 p-2 gap-4">
-        <Cards />
+        <Cards 
+          link="https://trip-booking.vercel.app/"
+          imgSrc="/assets/Project1.png"
+          title="Trip Booking Page"
+        />
+        <Cards 
+          link="https://film-directory.vercel.app/"
+          imgSrc="/assets/Project2.png"
+          title="Film Directory using API"
+        />
       </div>
     </div>
   );
